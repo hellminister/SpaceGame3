6844946f48892fa51114e671be79bf83aa0abb34
Grace Hudgens <54286731+Grace0Hud@users.noreply.github.com>
2020-10-31 07:59:12 -0700

    Documentation for SpaceGame class (#4)
    * documentation for SpaceGame class
    
    * Update misc.xml
    
    * SpaceGame documentation updated
    
    * Update misc.xml

--- src/spacegame3/SpaceGame.java
+++ src/spacegame3/SpaceGame.java
@@ -1,5 +1,4 @@
 package spacegame3;
-
 import javafx.application.Application;
 import javafx.stage.Stage;
 import spacegame3.gamedata.GameScheme;
@@ -15,7 +14,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
-
 public final class SpaceGame extends Application {
     private static final Logger LOG = Logger.getLogger(SpaceGame.class.getName());
 
@@ -29,8 +27,9 @@ public final class SpaceGame extends Application {
             LOG.info(e::toString);
         }
     }
-
-
+    /*
+        *Instance variables
+    */
     private final StartScreen startScreen;
     private final PlanetScreen planetScreen;
 
@@ -42,22 +41,35 @@ public final class SpaceGame extends Application {
 
     private boolean gameStarted;
 
-    public SpaceGame() {
+    /**
+     *links startScreen and planetScreen to this context
+    */
+    public SpaceGame()
+    {
         startScreen = new StartScreen(this);
         planetScreen = new PlanetScreen(this);
         gameScheme = null;
         gameStarted = false;
     }
 
+    //game scheme getter
     public GameScheme getGameScheme() {
         return gameScheme;
     }
 
+    /**
+     * sets the current GameScheme to a new one
+     * updates the ImageLibrary with a new story path from the gamescheme
+     */
     public void setGameScheme(GameScheme gameScheme) {
         this.gameScheme = gameScheme;
         ImageLibrary.setStoryTellingPath(gameScheme.getStoryPath());
     }
 
+    /**
+        *starts up the the game
+        *creates title and scene + shows
+    **/
     @Override
     public void start(Stage primaryStage) {
         stage = primaryStage;
@@ -74,12 +86,20 @@ public final class SpaceGame extends Application {
         planetScreen.setSize(stage.getWidth(), stage.getHeight());
     }
 
+    /**
+     * takes in a SizeableScene and sizes it to the stage
+     * refreshes the scene then sets the stage scene to the scene
+     */
     public void giveSceneTo(SizableScene scene){
         scene.setSize(stage.getWidth(), stage.getHeight());
         scene.refresh();
         stage.setScene(scene);
     }
 
+    /**
+     * main method - runs + launches game
+     * takes in String[] args
+    **/
     public static void main(String[] args) {
         launch(args);
     }
@@ -88,12 +108,19 @@ public final class SpaceGame extends Application {
         return gameStarted;
     }
 
-
+    /**
+     *sets the planet screen to have landed on a given CelestialBody
+     *returns planet screen
+    */
     public SizableScene getPlanetScreen(CelestialBody planet) {
         planetScreen.setLandedOn(planet);
         return planetScreen;
     }
 
+    /**
+     * stores the current scene
+     * transfers to the start screen
+    */
     public void showStartScreen(SizableScene scene) {
         previousScene = scene;
         giveSceneTo(startScreen);
@@ -103,6 +130,11 @@ public final class SpaceGame extends Application {
         return previousScene;
     }
 
+    /**
+     * gets the current CelestialBody a player is on
+     * if that CelestialBody is not null then the scene is set for that celestial body
+     * if it is null, the StarSystem the player is in is found and,
+    */
     public void sendToRightScene() {
         CelestialBody cb = gameScheme.getGameState().getPlanet();
         if (cb != null){
